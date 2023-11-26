daa4f2346b8e36622323b2ea871a02686e6011a0
Reese G <rm9eor9e@gmail.com>
2023-11-25 16:12:21 -0800

    restructured menu element interfaces, hoverable is its own catagory now.

--- MenuInput.java
+++ MenuInput.java
@@ -1,16 +1,29 @@
 public class MenuInput {
-    public int x, y;
-    public boolean select;
+
+    private int x, y;
+    private boolean select;
+
     public MenuInput() {
         update(0, 0, false);
     }
     public MenuInput(int x, int y, boolean select) {
         update(x, y, select);
     }
+
     public MenuInput update(int x, int y, boolean select) {
         this.x = x;
         this.y = y;
         this.select = select;
         return this;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public boolean getSelect() {
+        return this.select;
     }
 }
